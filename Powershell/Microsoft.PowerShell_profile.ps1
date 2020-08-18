702e1267cc97d736eac63ea06bc3113c8b0170a0
CA <chris_g_armstrong@hotmail.com>
2020-08-18 17:34:28 +1000

    Added .zshrc from wsl & added Powershell powerline config & Windows Terminal Config

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+# 18/08/2020 CA Config File for Setting up powerline with Powershell
+# Works with WindowsTerminal
+# Followed the instructions from https://www.hanselman.com/blog/HowToMakeAPrettyPromptInWindowsTerminalWithPowerlineNerdFontsCascadiaCodeWSLAndOhmyposh.aspx
+# This file is found by typing notepad $PROFILE
+# Easiest way to implement is to do a link using this source in DOS
+# C:\Users\guts>mklink /H D:\Source\Personal\Utilities\Powershell\Microsoft.PowerShell_profile.ps1 D:\Users\guts\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# Hardlink created for D:\Source\Personal\Utilities\Powershell\Microsoft.PowerShell_profile.ps1 <<===>> D:\Users\guts\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+
+
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
+
+Screenfetch
